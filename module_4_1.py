fe14e7ff457adb6d7ac0d0bf766d889c2f904c14
Nyzlo4ka <nyzlo4ka@mail.ru>
2024-06-30 15:24:35 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,8 @@
+from fake_math import divide as fake_d
+from true_math import divide as true_d
+
+result1 = print(fake_d(69, 3))
+result2 = print(fake_d(3, 0))
+result3 = print(true_d(49, 7))
+result4 = print(true_d(15, 0))
+
